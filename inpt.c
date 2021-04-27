fa52024ff25fb7a09cab1bb687443b45a998cdf8
RaghavendraReddy960 <59761335+RaghavendraReddy960@users.noreply.github.com>
2021-04-27 18:05:08 +0530

    Create inpt.c

---
+++ inpt.c
@@ -0,0 +1,45 @@
+#include<stdio.h> 
+#include<string.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/types.h> 
+#include<sys/wait.h> 
+#include<readline/readline.h> 
+#include<readline/history.h> 
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+void init(){
+    
+    printf("\n\n\n**************************************\n\n\n\n");
+    printf(ANSI_COLOR_BLUE "\n\t\tAssignment Shell\n" ANSI_COLOR_RESET);
+    printf("\n\n\n**************************************\n\n\n\n");
+    sleep(1);
+    
+}
+
+//void tokenizer
+
+int main(){
+    init();
+    char *user = getenv("USER");
+    long long int i=0;
+    while(i<1000){
+        char *inpt;
+        char dire[1024]; 
+         getcwd(dire, sizeof(dire));
+        printf(ANSI_COLOR_RED "%s" ANSI_COLOR_RESET, user);
+        printf(ANSI_COLOR_GREEN "%s" ANSI_COLOR_RESET, dire);
+        inpt = readline("/>");
+        add_history(inpt);
+        i++;
+        //tokenizer(inpt); send this inpt string to tokenizer
+    }
+    return 0;
+}
