30c7343ba78bb9b59aa58d099b5a10419e84c9f9
Sneha-223 <sneha.raghavaraju@students.iiit.ac.in>
2021-05-08 12:56:19 +0530

    Added submit function

--- commands/submit.c
+++ commands/submit.c
@@ -1,1 +1,52 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <dirent.h>
+#include <errno.h>
+
+#include <string.h>
+
+#include <stdio.h>
+
+//**********************************************************************************//
+// FUNCTION DESCRIPTION
+// Submit function takes the Assignment folder name as input
+// Zips the folder and stores it with the name 'AssignmentZip.zip', in the local directory
+
+// USAGE: submit <assignmentFolderName>
+// eg: submit Assignment
+
+// TODO:
+// add error handling (input validation, etc)
+// add output messages for the user 
+//******************************************************************************//
+
+
+void submit(char assignmentFolderName[])
+{
+
+    //zip the Assignment folder
+    char zipCommand[80];
+    strcpy(zipCommand, "zip -r ");
+    strcat(zipCommand, "AssignmentZip.zip ");
+    strcat(zipCommand, assignmentFolderName);    //eg: "zip -r AssignmentZip.zip Assignment";
+
+    system(zipCommand);
+
+}
+
+
+// //main function for testing
+// int main()
+// {
+//     char assignmentFolderName[40];
+
+//     //take input string, for the file name.
+//     scanf("%s", assignmentFolderName);
+    
+//     submit(assignmentFolderName);
+
+//     return 0;
+// }
+
 
