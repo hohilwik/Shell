a78d99474a0e9051737446ec68ed1f9a084de27e
RaghavendraReddy960 <59761335+RaghavendraReddy960@users.noreply.github.com>
2021-05-08 17:33:30 +0530

    temporary create files not finilised

---
+++ create.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include "create.h"
+
+void createdir(char dirname[])
+{
+    char directory[1024];
+    DIR* direc = opendir(dirname);
+    if(direc){
+        //need to use the update command here 
+        printf("direcory exists calling update function");
+    }
+    else if(direc==NULL){
+        strcpy(directory, "mkdir ");
+        strcat(directory, dirname);
+        system(directory);
+        char change[1024];
+        strcpy(change,"cd ");
+        strcat(change, dirname);
+        system(change);
+    }
+
+
+}
+
+/*int main(){
+    //parer and tokenizer value need to be passed to the dirname[]
+    //for testing purpose sending this example dir name(DSA)
+    char direc[1024] = "DSA";
+    createdir(direc);    
+    return 0;
+}*/
---
+++ create.h
@@ -0,0 +1,6 @@
+#ifndef __CRT_H
+#define __CRT_H
+
+void createdir(char dirname[]);
+
+#endif
