4ebc8ca55523059d397ca7f8390c1f7b6a0a2ef4
Sneha-223 <71211261+Sneha-223@users.noreply.github.com>
2021-05-09 18:46:46 +0530

    Update directoryfunc.h

--- directoryfunc/directoryfunc.h
+++ directoryfunc/directoryfunc.h
@@ -1,1 +1,12 @@
+#ifndef __DIRFUNC_
+#define __DIRFUNC_
+
+//copyDir
+void copyDir(char *sourceDir, char *targetDir);
+
+//createDir
+int doesFileExist(const char *path);
+int createDir(char *dirname);
+
+#endif
 
