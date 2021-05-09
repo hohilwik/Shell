8542cb3011ce04304aa3a7e04b0cb07fdd1f9b32
Shirley <80057848+hohilwik@users.noreply.github.com>
2021-05-10 00:18:46 +0530

    Update output.c

--- io/output.c
+++ io/output.c
@@ -23,9 +23,46 @@
 
 void init(){
     
-    printf("\n\n\n**************************************\n\n\n\n");
+printf(ANSI_COLOR_CYAN"                                               ..,.oooE777999V(;\n");
+printf(ANSI_COLOR_CYAN"                                  ...oooP779090(;''       ''''  I\n");
+printf(ANSI_COLOR_CYAN"                    ...ooB777979V;;''       .....=v}}=}=}=}}v==  5\n");
+printf(ANSI_COLOR_CYAN"               97?(;''     .........<<vvvv<vvvvvvvvvvvvvvv}}}}v} L\n");
+printf(ANSI_COLOR_CYAN"               ,   ..;;`;;;;;<;<<<<<<<<<<<<<v<vvvvvvvvvv}vv}}}}}. 1\n");
+printf(ANSI_COLOR_CYAN"               b (:::``;;;;;;;;;;;<;<;<<<<<<<<<<<<v<v<vvvvvvvv}v}, E\n");
+printf(ANSI_COLOR_CYAN"               `J ::.:.:.``;;;;;;;;;;;<;;;<<<<<<<<<<<<v<v<vvvvvvvx L\n");
+printf(ANSI_COLOR_CYAN"                L  :. :.:.:.:.``;;;;;;;;;;;;;;<;<<<<<<<<<<v<<v<vv<( T\n");
+printf(ANSI_COLOR_CYAN"                `> .    . .:.:.:.:.`:;``;;;;;;;;<;;;<;<<<<<<<<<<<v< >\n");
+printf(ANSI_COLOR_CYAN"                 b ;           . : .:.:.:.`;;;;;;;;;;;<;;<;<<<<<<<<, I\n");
+printf(ANSI_COLOR_CYAN"                 `,`               . : :.:.:.:.`.`;;;;;;;;;;;;<;<;<<. 5\n");
+printf(ANSI_COLOR_CYAN"                  b ;                    . : .:.:.:.`;;;;;;;;;;;<;<;<: E\n");
+printf(ANSI_COLOR_CYAN"                  `,<                         . . .:.:.:.``;;;;;;;;;;. I\n");
+printf(ANSI_COLOR_CYAN"                   b :                             . . :.:.:.:.:.:.;;;. 5\n");
+printf(ANSI_COLOR_CYAN"                   `>;                                  . .:..:.:.:.`.:  |\n");
+printf(ANSI_COLOR_CYAN"                    b :                                      . . :.:.:.x T\n");
+printf(ANSI_COLOR_CYAN"                    `,;                                          . . .::  E\n");
+printf(ANSI_COLOR_CYAN"                     b :                                               _  !4\n");
+printf(ANSI_COLOR_CYAN"                     `r :                                   __.__,--,;'))))).\n");
+printf(ANSI_COLOR_CYAN"                      b :                         ___...--'; `))))))))' '' `>!9eOc\n");
+printf(ANSI_COLOR_CYAN"                      `r :              __,--:-;;;)))))))))))'' '' ' ' _. -'-'.`!9Eg.\n");
+printf(ANSI_COLOR_CYAN"                       L : . __.--_--:,)))))))))))'' ' '  _. ._.-'-'-'-'\-'\---\/\ ``Qu.\n");
+printf(ANSI_COLOR_CYAN"                       `,: !x;:)))))))) ')'' ' _ _._-.'\'\_\_-'\''-\'_'\-'\'\ -_\'-\-. 95n.\n");
+printf(ANSI_COLOR_CYAN"                        D` ))))'''  _ .___.-_:/-/\/-_\ /-_, /-,\ \-/_\/\,-\_/-\/-/--' ..v<]9o.\n");
+printf(ANSI_COLOR_CYAN"                      __b :<> -_\._/\,- ,_ -\ _/\-\ _-\ -_/-\,\/,-/\_/-_\'\--' .vvvvvvv}v}}x}]NEo.\n");
+printf(ANSI_COLOR_CYAN"                .ooPO%LOCu  `< `/\_ -:\/_/-/,\/,/-,/_,-/\ :_\:_-:__-'' ...vvvvvvvvvvvvxx}vx}}}}==No\n");
+printf(ANSI_COLOR_CYAN"              .oPO'       `y. `< ~-\ _\/\_,- \ , - ,___..--' .......>>vvvvvvvvx<xvvxx}=x===}~^^   I\n");
+printf(ANSI_COLOR_CYAN"        om3jR&57'          `Ey, `\ `!,\ \-/_/\_---''.......vv>>vvvvvvvvvv)v<xvx=}=<~~^~`       :_yd\n");
+printf(ANSI_COLOR_CYAN"    _.rq8'                    `L, `<_ `--'.......vv<<<<v<<<<x<vv<vvvvxxxx=>~~~`         iuuuaE'\n");
+printf(ANSI_COLOR_CYAN"  .@tTL'                        `y,  `< .-vvv<<<<<<<<<xxvx>vvvvv=>~~~~`         _uuua'''\n");
+printf(ANSI_COLOR_CYAN".&P'                              `L,  `>>><<<<><>v<vvvvvx~`::`       ::_uuua'''\n");
+printf(ANSI_COLOR_CYAN"                                    `y,  `:F_P:<x>~>^` `        _uuug'\n");
+printf(ANSI_COLOR_CYAN"                                      `L,  ~~`          _uuua''\n");
+printf(ANSI_COLOR_CYAN"                                        `L,:    _uuua''\n");
+printf(ANSI_COLOR_CYAN"                                          `LaE''\n"ANSI_COLOR_RESET);
+
+
+    printf("\n**************************************\n");
     printf(ANSI_COLOR_BLUE "\n\t\tAssignment Shell\n" ANSI_COLOR_RESET);
-    printf("\n\n\n**************************************\n\n\n\n");
+    printf("\n**************************************\n");
     sleep(1);
     
 }
