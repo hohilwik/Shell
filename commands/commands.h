3e44275fd6d5577a4aadda73c6f31dda034ad79d
Sneha-223 <sneha.raghavaraju@students.iiit.ac.in>
2021-05-09 15:39:36 +0530

    Updated commands.h

--- commands/commands.h
+++ commands/commands.h
@@ -1,1 +1,25 @@
+#ifndef __COMMANDS_
+#define __COMMANDS_
 
+
+//compare function
+void makeAuxDirectory();
+void compare(char assignmentFolderName[], char zipFolderName[]);
+
+//submit function
+void submit(char assignmentFolderName[]);
+
+//switch function
+int switchdir(char *directory);
+
+//test function
+int doesFileExist(const char* path);
+char* DirPath();
+int Test_file(char *fileName);
+
+//use function
+void use(char assignmentFolderName[]);
+void getPath(char* path);
+
+
+#endif
