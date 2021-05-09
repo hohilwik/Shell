edc7ffa081be05c2a94b4a9090e9e75c1eaffb15
Shirley <80057848+hohilwik@users.noreply.github.com>
2021-05-09 09:53:43 +0530

    Update switch.c

--- commands/switch.c
+++ commands/switch.c
@@ -1,1 +1,14 @@
+int switchdir(char *directory)
+{
+	int flag;
+  //go to parent directory
+	chdir("..");
+	char temp[1024]="./";
+	strcat(temp, directory);
+  //now switch to the directory
+	flag=chdir(temp);
+	return flag;
+	//0 if successful
+	//-1 otherwise
 
+}
