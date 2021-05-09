f396b149cb9cce0e5cd4a0ce0527f4abb6e7c2e4
Sneha-223 <71211261+Sneha-223@users.noreply.github.com>
2021-05-09 20:23:12 +0530

    Update directoryfunc.h

--- directoryfunc/directoryfunc.h
+++ directoryfunc/directoryfunc.h
@@ -5,8 +5,8 @@
 void copyDir(char *sourceDir, char *targetDir);
 
 //createDir
-int doesFileExist(const char *path);
-int createDir(char *dirname);
+int doesFileExist(const char *path);  //returns -1 if it doesn't exist
+int createDir(char *dirname);   //if successful, return 0, if permission denied or name too long, return -1, if directory already exists, return 1
 
 #endif
 
